f35a8f6b3c5b73f86088f4240b4bcf4f0ab583a2
rikinp <30802344+rikinp@users.noreply.github.com>
2017-08-15 01:36:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var object = document.getElementById('main-text');
 
 object.innerHTML = 'New value'; 
+
+var img = document.getElementById('img');
+img.onclick = function () {
+    img.stylemarginleft = '100px';
+};
